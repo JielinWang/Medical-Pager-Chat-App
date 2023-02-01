7ad3b4fc5144dd1635db251ec984e74b330cb130
jielin Wang <wangjielin777@gmail.com>
2023-02-01 00:22:07 -0600

    commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import { ChannelListContainer, ChannelContainer } from "./components";
+import "./App.css";
+
+const apiKey = "csxbhebrzr6a";
+
+const client = StreamChat.getInstance(apiKey);
+
+export const App = () => {
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+};
+
+export default App;
